896db8be14b04c5a39792ffe7cfd603706b91227
RebeccaHisey <11rjh10@queensu.ca>
2020-08-11 19:53:00 -0400

    updated to compute mAP instead of accuracy

--- EvaluateFRCNN.py
+++ EvaluateFRCNN.py
@@ -6,6 +6,7 @@ import cv2
 import sklearn.metrics
 from matplotlib import pyplot as plt
 import pandas
+from scipy import optimize
 import SimpleITK
 
 def readCSV(videoName):
@@ -36,6 +37,155 @@ def convertNametoIndex(name):
         index  = 8
     return index
 
+def getIOU(bbox1,bbox2):
+    [xmin1,ymin1,xmax1,ymax1] = bbox1
+    [xmin2, ymin2, xmax2, ymax2] = bbox2
+    x_left = max(xmin1,xmin2)
+    x_right = min(xmax1,xmax2)
+    y_top = max(ymin1,ymin2)
+    y_bottom = min(ymax1,ymax2)
+
+    intersection = (x_right - x_left)*(y_bottom-y_top)
+    bbox1_area = (xmax1-xmin1)*(ymax1-ymin1)
+    bbox2_area = (xmax2 - xmin2) * (ymax2 - ymin2)
+    union = float(bbox1_area + bbox2_area - intersection)
+
+    intersectionOverUnion = intersection / union
+    return intersectionOverUnion
+
+def smoothPrecision(precision):
+    for i in range(0,len(precision)):
+        precision[i] = max(precision[i:])
+    return precision
+
+def getMAP(trueLabels,predLabels):
+    toolAcronyms = ['a','c','d','g','gc','sc','sy']
+    tools = ['anesthetic','catheter','dilator','guidewire','guidewire_casing','scalpel','syringe']
+    classAP = []
+    toolPrecision =[]
+    toolRecall = []
+    j=0
+    for tool in tools:
+        precision = []
+        recall = []
+        (rankedSamples,totalPosSamples) = getRankedToolPredictions(trueLabels,predLabels,tool,toolAcronyms[j])
+        j+=1
+        truePositives = 0
+        falsePositives = 0
+        falseNegatives = 0
+        for i in range(len(rankedSamples)):
+            if rankedSamples['true'][i] == 1 and rankedSamples['pred'][i]==1:
+                truePositives += 1
+            elif rankedSamples['true'][i] == 0 and rankedSamples['pred'][i] == 1:
+                falsePositives += 1
+            elif rankedSamples['true'][i] == 1 and rankedSamples['pred'][i] == 0:
+                falseNegatives += 1
+            precision.append(truePositives / float(truePositives + falsePositives))
+            recall.append(truePositives / float(totalPosSamples))
+
+        smoothedPrecision = numpy.array(smoothPrecision(precision))
+        recall = numpy.array(recall)
+        if precision !=[]:
+            #
+            auc = sklearn.metrics.auc(recall, numpy.array(precision))
+        else:
+            auc = 0.0
+        toolPrecision.append(smoothedPrecision)
+        toolRecall.append(recall)
+        classAP.append(auc)
+        print('{}: {}'.format(tool,auc))
+    plt.plot(toolRecall[0],toolPrecision[0], 'm', label='anesthetic')
+    plt.plot(toolRecall[1], toolPrecision[1], 'r', label='catheter')
+    plt.plot(toolRecall[2], toolPrecision[2], 'g', label='dilator')
+    plt.plot(toolRecall[3], toolPrecision[3], 'b', label='guidewire')
+    plt.plot(toolRecall[4], toolPrecision[4], 'c', label='guidewire_casing')
+    plt.plot(toolRecall[5], toolPrecision[5], 'y', label='scalpel')
+    plt.plot(toolRecall[6], toolPrecision[6], 'k', label='syringe')
+    plt.title('Precision Recall Curve')
+    plt.xlabel('recall')
+    plt.ylabel('precision')
+    plt.legend()
+    plt.show()
+    meanAP = numpy.mean(numpy.array(classAP))
+    print('mAP: {}'.format(meanAP))
+    return (meanAP, classAP)
+
+def getEqualTrueAndPred(trueLabels,predLabels):
+    trueAndPredLabels = pandas.DataFrame(columns=['true','pred','x_min_true','y_min_true','x_max_true','y_max_true','x_min_pred','y_min_pred','x_max_pred','y_max_pred','a','c','d','g','gc','sc','sy','bg'])
+    checkedImages = []
+    for i in range(len(predLabels)):
+        currentFile = predLabels['filePath'][i]
+        if not currentFile in checkedImages:
+            checkedImages.append(currentFile)
+            groundTruthBoxes = trueLabels.loc[trueLabels['filePath']== currentFile]
+            predBoxes = predLabels.loc[predLabels['filePath']==currentFile]
+            groundTruthIndices = groundTruthBoxes.index
+            predBoxIndices = predBoxes.index
+            for j in predBoxIndices:
+                Found = False
+                for k in groundTruthIndices:
+                    if predBoxes['label'][j] == groundTruthBoxes['class_name'][k]:
+                        Found = True
+                        trueAndPredLabels = trueAndPredLabels.append({'true':groundTruthBoxes['class_name'][k],'pred':predBoxes['label'][j],'x_min_true':groundTruthBoxes['x_min'][k],'y_min_true':groundTruthBoxes['y_min'][k],'x_max_true':groundTruthBoxes['x_max'][k],'y_max_true':groundTruthBoxes['y_max'][k],'x_min_pred':predBoxes['xmin'][j],'y_min_pred':predBoxes['ymin'][j],'x_max_pred':predBoxes['xmax'][j],'y_max_pred':predBoxes['ymax'][j],'a':predBoxes['a'][j],'c':predBoxes['c'][j],'d':predBoxes['d'][j],'g':predBoxes['g'][j],'gc':predBoxes['gc'][j],'sc':predBoxes['sc'][j],'sy':predBoxes['sy'][j],'bg':predBoxes['bg'][j]},ignore_index=True)
+                if not Found:
+                    trueAndPredLabels = trueAndPredLabels.append(
+                        {'true': 'none', 'pred': predBoxes['label'][j],
+                         'x_min_true': 0, 'y_min_true': 0,
+                         'x_max_true': 0, 'y_max_true': 0,
+                         'x_min_pred': predBoxes['xmin'][j], 'y_min_pred': predBoxes['ymin'][j],
+                         'x_max_pred': predBoxes['xmax'][j], 'y_max_pred': predBoxes['ymax'][j],
+                         'a': predBoxes['a'][j], 'c': predBoxes['c'][j], 'd': predBoxes['d'][j], 'g': predBoxes['g'][j],
+                         'gc': predBoxes['gc'][j], 'sc': predBoxes['sc'][j], 'sy': predBoxes['sy'][j],
+                         'bg': predBoxes['bg'][j]},ignore_index=True)
+            for j in groundTruthIndices:
+                Found = False
+                for k in predBoxIndices:
+                    if predBoxes['label'][k] == groundTruthBoxes['class_name'][j]:
+                        Found = True
+                if not Found:
+                    trueAndPredLabels = trueAndPredLabels.append(
+                        {'true': groundTruthBoxes['class_name'][j], 'pred': 'none',
+                         'x_min_true': groundTruthBoxes['x_min'][j], 'y_min_true': groundTruthBoxes['y_min'][j],
+                         'x_max_true': groundTruthBoxes['x_max'][j], 'y_max_true': groundTruthBoxes['y_max'][j],
+                         'x_min_pred': 0, 'y_min_pred': 0,
+                         'x_max_pred': 0, 'y_max_pred': 0,
+                         'a': 0, 'c': 0, 'd': 0, 'g': 0,
+                         'gc': 0, 'sc': 0, 'sy': 0,
+                         'bg': 1},ignore_index=True)
+    return trueAndPredLabels
+
+
+def getRankedToolPredictions(trueLabels,predLabels,toolName,toolAcronym):
+    samples = pandas.DataFrame(columns=['true','pred','confidence'])
+    trueAndPredLabels = getEqualTrueAndPred(trueLabels,predLabels)
+    numPositiveSamples = 0
+    for i in range(len(trueAndPredLabels)):
+        if trueAndPredLabels['true'][i] == trueAndPredLabels['pred'][i] and trueAndPredLabels['true'][i] == toolName:
+                numPositiveSamples += 1
+                trueBBox = [trueAndPredLabels['x_min_true'][i], trueAndPredLabels['y_min_true'][i],
+                            trueAndPredLabels['x_max_true'][i], trueAndPredLabels['y_max_true'][i]]
+                predBBox = [trueAndPredLabels['x_min_pred'][i], trueAndPredLabels['y_min_pred'][i], trueAndPredLabels['x_max_pred'][i],
+                            trueAndPredLabels['y_max_pred'][i]]
+                iou = getIOU(trueBBox, predBBox)
+                if iou > 0.5:
+                    samples = samples.append({'true':1,'pred':1,'confidence':trueAndPredLabels[toolAcronym][i]},ignore_index=True)
+                else:
+                    samples = samples.append({'true':1,'pred':0, 'condfidence': trueAndPredLabels[toolAcronym][i]}, ignore_index=True)
+        elif trueAndPredLabels['true'][i] != trueAndPredLabels['pred'][i] and trueAndPredLabels['pred'][i] == toolName:
+                numPositiveSamples += 1
+                samples = samples.append({'true':0,'pred':1, 'condfidence': trueAndPredLabels[toolAcronym][i]}, ignore_index=True)
+        elif trueAndPredLabels['true'][i] != trueAndPredLabels['pred'][i] and trueAndPredLabels['true'][i] == toolName:
+            samples = samples.append({'true': 1, 'pred': 0, 'condfidence': trueAndPredLabels[toolAcronym][i]},
+                                     ignore_index=True)
+        else:
+            samples = samples.append({'true':0,'pred':0, 'condfidence': trueAndPredLabels[toolAcronym][i]}, ignore_index=True)
+    samples = samples.sort_values(by=['confidence'],ascending=False)
+    predRowIndexes = samples.index
+    rankedSamples = pandas.DataFrame(columns=['true','pred','confidence'])
+    for i in predRowIndexes:
+        rankedSamples = rankedSamples.append({'true':samples['true'][i],'pred':samples['pred'][i],'confidence':samples['confidence'][i]}, ignore_index=True)
+    return (rankedSamples,numPositiveSamples)
+
 def getAccuracy(trueLabels,predictedLabels):
     tLabels =[]
     pLabels =[]
@@ -49,10 +199,18 @@ def getAccuracy(trueLabels,predictedLabels):
             trueRowIndexes = allTrueBBoxes.index
             predRowIndexes = allPredBBoxes.index
             if len(predRowIndexes)> len(trueRowIndexes):
+                #match bboxes up based on iou
+                #if bboxes don't meet minimum iou requirements, then label as none
                 for j in predRowIndexes:
                     Plabel = allPredBBoxes['label'][j]
                     Tlabel = 'none'
+                    predbbox = [allPredBBoxes['x_min'][j],allPredBBoxes['y_min'][j],allPredBBoxes['x_max'][j],allPredBBoxes['y_max'][j]]
+                    bestIOU = 0
                     for k in trueRowIndexes:
+                        truebbox = [allTrueBBoxes['x_min'][k], allTrueBBoxes['y_min'][k], allTrueBBoxes['x_max'][k],allTrueBBoxes['y_max'][k]]
+                        iou = getIOU(predbbox,truebbox)
+                        if iou > bestIOU:
+                            pass
                         if allTrueBBoxes['class_name'][k] == Plabel:
                             Tlabel = allTrueBBoxes['class_name'][k]
                     Tlabel = convertNametoIndex(Tlabel)
@@ -166,8 +324,10 @@ def main():
     for i in range(len(videos)):
         print(videos[i])
         trueLabels,predictedLabels = readCSV(videos[i])
-        (accuracy,totalFrameCount) = getAccuracy(trueLabels,predictedLabels)
-        print (accuracy)
+        tool = 'anesthetic'
+        getMAP(trueLabels,predictedLabels)
+        #(accuracy,totalFrameCount) = getAccuracy(trueLabels,predictedLabels)
+        #print (accuracy)
         '''
         (accuracy, truePositives, trueNegatives, falsePositives, falseNegatives, totalFrameCount) = getAccuracy(trueLabels,predictedLabels)
         (meanDistance,stdDev) = getMeanDistance(trueLabels,predictedLabels)
